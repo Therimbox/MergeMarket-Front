7878ddf489e4b6db7aaa22f25b1f49de493d02d9
Therimbox <alexandreferi13@gmail.com>
2025-11-03 15:05:40 +0100

    api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: 'http://84.246.213.41:8080',
 });
 
 export default api;
